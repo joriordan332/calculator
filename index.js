aacd49513976b7bac839715e858db5c420180c2f
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-01-24 16:06:36 +0000

    Added if statements to eventListeners Number eventListener has an added if statement added if user tries to divide zero by anything and displays a message if they try Equals eventListener has an if statement added in case user presses equals before an operator is pressed to avoid problems evaluating numbers

--- index.js
+++ index.js
@@ -9,40 +9,49 @@ let num1 = "";
 let num2 = "";
 let operator = "";
 
-
+//event listener that displays the numbers clicked on the calculator display apart from the operator buttons
 numbers.forEach(button => {
   button.addEventListener('click', (event) => {
     if (event.target.value == '+' || event.target.value == '-' || event.target.value == '/' || event.target.value == '*' || event.target.value == '=') {
     return false
-  } if (num1 !== null && num1 !== '') {
+  } if (num1 !== null && num1 !== '') { // if the variable num1 isn't empty then clear the textContent for the next number
       display.textContent = "";
       displayResult = display.textContent += event.target.value;
-  } if (num1 != "" && operator != "") {
+  } if (num1 != "" && operator != "") { // if num1 & the operator has a value then num2 is assigned the value of the displayResult
       num2 = displayResult
-  } if (num1 == null || num1 == ''){
+  } if (num1 == null || num1 == ''){  // if num1 doesn't have a value then the textContent is whichever button is clicked
       displayResult = display.textContent += event.target.value; 
-  } 
+  } if (num1 === "0" && operator === "/") {
+    alert("Nice try");
+    num1 = "";
+    operator = "";
+    display.textContent = "";
+  }
+
   });
 });
 
-
 operators.forEach(button => {
   button.addEventListener('click', (event) => {
-    if (displayResult === "") {
-      operator = "";
-    } if (operator === "" && displayResult != "") {
+    if (display.textContent === '') { //if an operator is clicked before a value is put in the text box no value is assigned to the variable
+      return false;
+    } if (operator === "" && displayResult != "") { // if an operator button is clicked and operator is empty and displayResult isn't empty then num1 is assigned the same value as displayResult
         num1 = displayResult;
     } if (num1 != "" && operator != "" && num2 != "") {
         num1 = display.textContent = (operate(operator, num1, num2))
-    }
+    } 
       operator = event.target.value;
    })
 })
 
 equalsButton.addEventListener('click', (event) => {
+  if (operator === "") {
+    displayResult = display.textContent
+  } if (num1 != "" && operator != "" && num2 != "") {
   num2 = displayResult
   display.textContent = (operate(operator, num1, num2))
   console.log(operate(operator, num1, num2))
+  }
 })
 
 clear.addEventListener('click', (event) => {
@@ -52,6 +61,7 @@ clear.addEventListener('click', (event) => {
   operator = "";
 })
 
+//functions for add, subtractm multiply & divide
 function add(...addNums) {
   let plus = addNums.reduce ((num1, num2) => (num1 + num2))
   return plus
@@ -72,6 +82,7 @@ function divide(...share) {
   return division;
 };
 
+//operate function that takes an operator and 2 numbers and then calls one of the above functions on the numbers
 function operate(operator, num1, num2){
   switch(operator) {            
     case '+':
@@ -88,4 +99,3 @@ function operate(operator, num1, num2){
       break;
   }
 };
-
