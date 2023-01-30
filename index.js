88948928e1490fd51555237a537dad241d09d5f9
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-01-30 14:57:17 +0000

    CSS properties added & decimal eventListener added
    CSS styles added so that calculator looks like an iPhone calculator
    Decimal eventListener added so that a user can only input a decimal once as the specs want

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 const numbers = document.querySelectorAll('.numbers');
 const display = document.querySelector('.display');
 const operators = document.querySelectorAll('.operators');
-const equalsButton = document.querySelector('.equalsButton')
-const clear = document.querySelector('.clear')
+const equalsButton = document.querySelector('.equalsButton');
+const clear = document.querySelector('.clear');
+const decimal = document.querySelector('.decimal')
 
 let displayResult = "";
-let num1 = "";
-let num2 = "";
+let currentNum = "";
+let previousNum = "";
 let operator = "";
+
 
 //event listener that displays the numbers clicked on the calculator display apart from the operator buttons
 numbers.forEach(button => {
   button.addEventListener('click', (event) => {
-    if (event.target.value == '+' || event.target.value == '-' || event.target.value == '/' || event.target.value == '*' || event.target.value == '=') {
+    if (event.target.value == '+' || event.target.value == '-' || event.target.value == '/' || event.target.value == '*' || event.target.value == '=' || event.target.value == '.'  ) {
     return false
-  } if (num1 !== null && num1 !== '') { // if the variable num1 isn't empty then clear the textContent for the next number
-      display.textContent = "";
+  } if (currentNum !== null && currentNum !== '' && display.textContent.length <= 8) { // if the variable currentNum isn't empty then clear the textContent for the next number
       displayResult = display.textContent += event.target.value;
-  } if (num1 != "" && operator != "") { // if num1 & the operator has a value then num2 is assigned the value of the displayResult
-      num2 = displayResult
-  } if (num1 == null || num1 == ''){  // if num1 doesn't have a value then the textContent is whichever button is clicked
+  } if (currentNum != "" && operator != "") { // if currentNum & the operator has a value then previousNum is assigned the value of the displayResult
+      previousNum = displayResult
+  } if (currentNum == null || currentNum == '' && display.textContent.length <= 8){  // if currentNum doesn't have a value then the textContent is whichever button is clicked
       displayResult = display.textContent += event.target.value; 
-  } if (num1 === "0" && operator === "/") { // if user tries to divide 0 by anything display a snarky error message
-    alert("Nice try");
-    num1 = "";
+  } if (operator === "/" && previousNum === "0" ) { // if user tries to divide 0 by anything display a snarky error message
+    alert("You can't divide by zero");
+    currentNum = "";
+    previousNum = ""
     operator = "";
     display.textContent = "";
   }
-
   });
 });
 
@@ -35,67 +36,78 @@ operators.forEach(button => {
   button.addEventListener('click', (event) => {
     if (display.textContent === '') { //if an operator is clicked before a value is put in the text box no value is assigned to the variable
       return false;
-    } if (operator === "" && displayResult != "") { // if an operator button is clicked and operator is empty and displayResult isn't empty then num1 is assigned the same value as displayResult
-        num1 = displayResult;
-    } if (num1 != "" && operator != "" && num2 != "") {
-        num1 = display.textContent = (operate(operator, num1, num2))
+    } if (operator === "" && displayResult != "") { // if an operator button is clicked and operator is empty and displayResult isn't empty then currentNum is assigned the same value as displayResult
+        currentNum = displayResult;
+    } if (currentNum != "" && operator != "" && previousNum != "") {
+        currentNum = display.textContent = (operate(operator, currentNum, previousNum))
     } 
       operator = event.target.value;
+      display.textContent = "";
    })
 })
 
 equalsButton.addEventListener('click', (event) => {
   if (operator === "") {
     displayResult = display.textContent
-  } if (num1 != "" && operator != "" && num2 != "") {
-  num2 = displayResult
-  display.textContent = (operate(operator, num1, num2)).toFixed(2)
-  console.log(operate(operator, num1, num2))
+  } if (currentNum != "" && operator != "" && previousNum != "") {
+  previousNum = displayResult
+  display.textContent = (operate(operator, currentNum, previousNum)).toFixed(2)
+  console.log(operate(operator, currentNum, previousNum))
   }
 })
 
 clear.addEventListener('click', (event) => {
   display.textContent  = "";
-  num1 = "";
-  num2 = "";
+  currentNum = "";
+  previousNum = "";
   operator = "";
 })
 
+decimal.addEventListener("click", () => {
+  addDecimal();
+});
+
+function addDecimal() {
+  if (!display.textContent.includes(".")) {
+    display.textContent += "."
+ }
+}
+
 //functions for add, subtractm multiply & divide
 function add(...addNums) {
-  let plus = addNums.reduce ((num1, num2) => (num1 + num2))
+  let plus = addNums.reduce ((currentNum, previousNum) => (currentNum + previousNum))
   return plus
 };
 
 function subtract(...subNums) {
-  let sub = subNums.reduce((num1, num2) => (num1 - num2))
+  let sub = subNums.reduce((currentNum, previousNum) => (currentNum - previousNum)) 
   return sub;
 };
 
 function multiply(...multi) {
-  let times = multi.reduce((num1, num2) => (num1 * num2))
+  let times = multi.reduce((currentNum, previousNum) => (currentNum * previousNum))
   return times;
 };
 
 function divide(...share) {
-  let division = share.reduce((num1, num2) => (num1 / num2))
+  let division = share.reduce((currentNum, previousNum) => (currentNum / previousNum))
   return division;
 };
 
 //operate function that takes an operator and 2 numbers and then calls one of the above functions on the numbers
-function operate(operator, num1, num2){
+function operate(operator, currentNum, previousNum){
   switch(operator) {            
     case '+':
-      return add(+num1, +num2);
+      return add(+currentNum, +previousNum);
       break;
     case '-':
-      return subtract(num1, num2);
+      return subtract(currentNum, previousNum);
       break;
     case '*':
-      return multiply(num1, num2);
+      return multiply(currentNum, previousNum);
       break;
     case '/':
-      return divide(num1, num2);
+      return divide(currentNum, previousNum);
       break;
   }
 };
